968c9c0e6b5f7cfadf1fa667a9c3634838043f87
Aml-Abbas <aml.abbas@hotmail.com>
2021-09-01 16:21:05 +0200

    initial comment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,5 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-responsive-sidebar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-responsive-sidebar');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-responsive-sidebar app is running!');
-  });
+ 
 });
