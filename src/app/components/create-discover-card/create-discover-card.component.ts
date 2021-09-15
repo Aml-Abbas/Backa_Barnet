38b45dab540665710f1752590b1f8e7c590f1dfe
Aml_Abbas <aml.abbas@hotmail.com>
2021-09-15 17:35:08 +0200

    ..

--- src/app/components/create-discover-card/create-discover-card.component.ts
+++ src/app/components/create-discover-card/create-discover-card.component.ts
@@ -3,20 +3,11 @@ import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   type: string;
-  describtion: string;
+  description: string;
+  color: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { type: 'OMSORG', describtion: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.'},
-  { type: 'TRYGGHET', describtion: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.'},
-  { type: 'MÅR BRA', describtion: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.'},
-  {type: 'FRITID', describtion: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.'},
-  { type: 'TILLHÖRIGHET',  describtion: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.'},
-  { type: 'ANSVARSTAGANDE',  describtion: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.'},
-  { type: 'RESPEKTERAS',  describtion: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.'},
-  { type: 'UTVECKLAS',  describtion: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.'},
-  
-];
+
 
 
 @Component({
@@ -25,8 +16,17 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./create-discover-card.component.scss']
 })
 export class CreateDiscoverCardComponent implements OnInit {
-  displayedColumns: string[] = ['type', 'describtion'];
-  dataSource = ELEMENT_DATA;
+  ELEMENT_DATA: PeriodicElement[] = [
+    { color: 'LightBlue', type: 'OMSORG', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.'},
+    { color: 'LightSalmon',type: 'TRYGGHET', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.'},
+    { color: 'LightSeaGreen',type: 'MÅR BRA', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.'},
+    {color: 'LightSteelBlue',type: 'FRITID', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.'},
+    { color: 'LightSlateGrey',type: 'TILLHÖRIGHET',  description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.'},
+    {color: 'LightGreen', type: 'ANSVARSTAGANDE',  description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.'},
+    {color: 'LightGoldenRodYellow', type: 'RESPEKTERAS',  description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.'},
+    { color: 'LightPink',type: 'UTVECKLAS',  description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.'},
+    
+  ];
 
   constructor() { }
 
