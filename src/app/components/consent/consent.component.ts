086d2c3e768bf2a11ac0730103a166809ae8b1ff
Aml_Abbas <aml.abbas@hotmail.com>
2021-09-16 15:43:06 +0200

    ..

--- src/app/components/consent/consent.component.ts
+++ src/app/components/consent/consent.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-consent',
   templateUrl: './consent.component.html',
   styleUrls: ['./consent.component.scss']
 })
 export class ConsentComponent implements OnInit {
+
 
   constructor() { }
 
