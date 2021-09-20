84bfae157c7a11359f63662b583e0207e6377dd4
Aml-Abbas <aml.abbas@hotmail.com>
2021-09-20 13:22:18 +0200

    ...

--- src/app/components/contact-person/contact-person.component.ts
+++ src/app/components/contact-person/contact-person.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-person.component.scss']
 })
 export class ContactPersonComponent implements OnInit {
+  namn = '';
+  personNumber = '';
+  address = '';
+  currentAddress = '';
+  postNumber = '';
+  currentPostNumber = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public save(): void {
+    console.log("namn: " +  this.namn);
+    console.log("personNumber: " +  this.personNumber);
+    console.log("address: " +  this.address);
+    console.log("currentAddress: " +  this.currentAddress);
+    console.log("postNumber: " +  this.postNumber);
+    console.log("currentPostNumber: " +  this.currentPostNumber);
+  }
 }
