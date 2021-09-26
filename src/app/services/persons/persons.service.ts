c408fca559d37ad8d49325d76e3d7f7fcf7c0ee1
Aml_Abbas <aml.abbas@hotmail.com>
2021-09-26 14:22:26 +0200

    ..

--- src/app/services/persons/persons.service.ts
+++ src/app/services/persons/persons.service.ts
@@ -8,24 +8,15 @@ import { Person } from 'src/app/models/Person';
 })
 export class PersonsService {
 
-  private persons_list= new BehaviorSubject<Person[]>([]);
-  private storedNames = JSON.parse(localStorage.getItem("personsList") || '[]');
-
   private current_person= new BehaviorSubject<Person>(new Person('','','','','','','','','','','',''));
   private storedPerson = JSON.parse(localStorage.getItem("currentPerson") || '[]');
 
   
-  current_persons_list$= this.persons_list.asObservable();
   current_person$= this.current_person.asObservable();
 
   constructor() {
-    this.persons_list.next(this.storedNames);
     this.current_person.next(this.storedPerson);
    }
-
-  setPersonList(persons: Person[]){
-    this.persons_list.next(persons);
-  }
 
    setPerson(person: Person){
     this.current_person.next(person);
--- src/app/services/sign-in/sign-in.service.ts
+++ src/app/services/sign-in/sign-in.service.ts
@@ -16,49 +16,6 @@ export class SignInService {
 
   signIn(info: LoginInfo): Observable<Person[]> {
 
-    return this.http.get<Person[]>('https://func-ykbb.azurewebsites.net/api/person/' + info.email + '/' + info.password + '?code=SkXpI51pgjWl6UVNjxKjKNUr3o2gmPdlOZ4EFMFwn0LR0KlyDlYu3w==')/* .subscribe(
-      (data)=>{
-
-        let int_ref: Person[] = [];
-
-        if (data instanceof Array) {
-          data.map(function(v, i) {
-          
-          let int_person: Person = new Person(
-                                      v.personID,
-                                      v.personNr,
-                                      v.firstName,
-                                      v.lastName,
-                                      v.address,
-                                      v.city,
-                                      v.personRoleID,
-                                      v.personTypeID,
-                                      v.createBy,
-                                      v.createDate,
-                                      v.changeBy,
-                                      v.changeDate
-                                      );    
-
-
-          int_ref.push(int_person);
-        })
-      }
-      console.log(int_ref);
-      console.log('int_ref');
-
-      this.persons_list = int_ref;
-      console.log(this.persons_list);
-      console.log('persons_list');
-
-      return this.persons_list;
-
-    });
-
-      console.log(this.persons_list);
-      console.log('persons_list');
-
-      return this.persons_list; */
+    return this.http.get<Person[]>('https://func-ykbb.azurewebsites.net/api/person/' + info.email + '/' + info.password + '?code=SkXpI51pgjWl6UVNjxKjKNUr3o2gmPdlOZ4EFMFwn0LR0KlyDlYu3w==');
   }
-
-
 }
