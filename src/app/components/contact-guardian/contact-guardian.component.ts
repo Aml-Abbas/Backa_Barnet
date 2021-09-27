7d856aceb77c517b71644e37f1d7140581c542ff
Aml-Abbas <aml.abbas@hotmail.com>
2021-09-27 15:27:24 +0200

    ...

--- src/app/components/contact-guardian/contact-guardian.component.ts
+++ src/app/components/contact-guardian/contact-guardian.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/Person';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromState from '../../state';
 
 @Component({
   selector: 'app-contact-guardian',
@@ -6,16 +10,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-guardian.component.scss']
 })
 export class ContactGuardianComponent implements OnInit {
-  contactName = '';
-  contactTask = '';
-  contactWorkPlace = '';
-  supporterName = '';
-  supporterTask = '';
-  supporterWorkPlace = '';
+  current_person$= new Observable<Person | null>();
 
-  constructor() { }
+  constructor(private store: Store<fromState.State>) { }
 
   ngOnInit(): void {
+    this.current_person$ = this.store.select(fromState.getCurrentPerson);
   }
 
 
