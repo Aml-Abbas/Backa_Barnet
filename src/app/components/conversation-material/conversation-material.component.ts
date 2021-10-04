00c6976d7e20809cefac9aa808f22fe742effc47
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-04 16:43:45 +0200

    ...

--- src/app/components/conversation-material/conversation-material.component.ts
+++ src/app/components/conversation-material/conversation-material.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversation-material.component.scss']
 })
 export class ConversationMaterialComponent implements OnInit {
-
+  care_score: string;
+  security_score: string;
+  feel_good_score: string;
+  free_time_score: string;
+  beloning_score: string;
+  responsibility_score: string;
+  respekt_score: string;
+  develop_score: string;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  changeCareScore(value: string){
+    this.care_score= value;
+    console.log(this.care_score);
+
+  }
+
+  save(): void{
+    console.log(this.care_score);
+    console.log(this.security_score);
+    console.log(this.feel_good_score);
+    console.log(this.free_time_score);
+    console.log(this.beloning_score);
+    console.log(this.responsibility_score);
+    console.log(this.respekt_score);
+    console.log(this.develop_score);
+  }
 }
