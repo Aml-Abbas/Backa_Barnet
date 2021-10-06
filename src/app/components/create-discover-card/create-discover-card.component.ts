daf5dae697aee5cc6558a97a0de92126099ea183
Aml-Abbas <aml.abbas@hotmail.com>
2021-10-06 09:25:04 +0200

    ...

--- src/app/components/create-discover-card/create-discover-card.component.ts
+++ src/app/components/create-discover-card/create-discover-card.component.ts
@@ -8,27 +8,21 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
 
 
-export interface PeriodicElement {
-  type: string;
-  description: string;
-  color: string;
-}
-
 @Component({
   selector: 'app-create-discover-card',
   templateUrl: './create-discover-card.component.html',
   styleUrls: ['./create-discover-card.component.scss']
 })
 export class CreateDiscoverCardComponent implements OnInit {
-  ELEMENT_DATA: PeriodicElement[] = [
-    { color: '#ccd8ec', type: 'OMSORG', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.'},
-    { color: '#dbd9e6',type: 'TRYGGHET', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.'},
-    { color: '#ffdcee',type: 'MÅR BRA', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.'},
-    {color: '#fbdae1',type: 'FRITID', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.'},
-    { color: '#fee4d7',type: 'TILLHÖRIGHET',  description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.'},
-    {color: '#fcedd6', type: 'ANSVARSTAGANDE',  description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.'},
-    {color: '#d9f2e4', type: 'RESPEKTERAS',  description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.'},
-    { color: '#d1f3f3',type: 'UTVECKLAS',  description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.'},
+  ELEMENT_DATA = [
+    { color: '#ccd8ec', type: 'OMSORG', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.', selected:'false'},
+    { color: '#dbd9e6',type: 'TRYGGHET', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.', selected:'false'},
+    { color: '#ffdcee',type: 'MÅR BRA', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.', selected:'false'},
+    {color: '#fbdae1',type: 'FRITID', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.', selected:'false'},
+    { color: '#fee4d7',type: 'TILLHÖRIGHET',  description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.', selected:'false'},
+    {color: '#fcedd6', type: 'ANSVARSTAGANDE',  description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.', selected:'false'},
+    {color: '#d9f2e4', type: 'RESPEKTERAS',  description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.', selected:'false'},
+    { color: '#d1f3f3',type: 'UTVECKLAS',  description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.', selected:'false'},
     
   ];
 
@@ -39,6 +33,7 @@ export class CreateDiscoverCardComponent implements OnInit {
   personNbr: string;
   adress: string;
   guardians: string[][]=[['',''],['','']];
+  comments: string[]=[];
 
   date: Date;
   discovererName: string;
@@ -49,7 +44,7 @@ export class CreateDiscoverCardComponent implements OnInit {
   situationComment: string;
 
   isMeasureTaken: boolean;
-  isMeasureTakenComment: string
+  isMeasureTakenComment: string;
 
   constructor(public dialog: MatDialog,
               private store: Store<fromState.State>) { 
@@ -77,6 +72,16 @@ export class CreateDiscoverCardComponent implements OnInit {
 
 }
 
+radioChangeThree(event: MatRadioChange, data) {
+
+  var obj = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];
+  console.log(obj);
+  
+  obj.selected = event.value;
+  console.log(this.ELEMENT_DATA);
+
+}
+
 
   openDialog() {
     /* console.log(this.date.toLocaleDateString());
@@ -89,8 +94,7 @@ export class CreateDiscoverCardComponent implements OnInit {
     console.log(this.adress);
      */
 
-    console.log('VALUE OF THE RADIO BUTTON IS:');
-    console.log();
+    console.log(this.comments);
 /*     console.log(this.guardians[0][0]);
     console.log(this.guardians[0][1]);
     console.log(this.guardians[1][0]);
