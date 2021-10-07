6019cc5c5b535af637098ccb8347f62fb425b2f2
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-07 09:26:59 +0200

    ...

--- src/app/components/create-discover-card/create-discover-card-dialog/create-discover-card-dialog.component.ts
+++ src/app/components/create-discover-card/create-discover-card-dialog/create-discover-card-dialog.component.ts
@@ -8,9 +8,19 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./create-discover-card-dialog.component.scss']
 })
 export class CreateDiscoverCardDialogComponent implements OnInit {
+  text:string;
+  status:boolean= true;
+  guardians:string[][];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string, personNbr: string, adress: string, guardianName1: string }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string, personNbr: string, guardians: [][] }) { 
+    this.guardians= data.guardians;
+  }
 
   ngOnInit(): void {
+    if(this.guardians[0][3]=='false'||this.guardians[1][3]=='false'){
+      this.text= 'kortet kommer att annomineras för det saknas samtycke';
+    }else{
+      this.text= 'kortet kommer att skickas in';
+    }
   }
 }
--- src/app/components/create-discover-card/create-discover-card.component.ts
+++ src/app/components/create-discover-card/create-discover-card.component.ts
@@ -19,11 +19,22 @@ export class CreateDiscoverCardComponent implements OnInit {
     { color: '#dbd9e6',type: 'TRYGGHET', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.', selected:'false'},
     { color: '#ffdcee',type: 'MÅR BRA', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.', selected:'false'},
     {color: '#fbdae1',type: 'FRITID', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.', selected:'false'},
-    { color: '#fee4d7',type: 'TILLHÖRIGHET',  description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.', selected:'false'},
-    {color: '#fcedd6', type: 'ANSVARSTAGANDE',  description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.', selected:'false'},
-    {color: '#d9f2e4', type: 'RESPEKTERAS',  description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.', selected:'false'},
-    { color: '#d1f3f3',type: 'UTVECKLAS',  description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.', selected:'false'},
+    { color: '#fee4d7',type: 'TILLHÖRIGHET', description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.', selected:'false'},
+    {color: '#fcedd6', type: 'ANSVARSTAGANDE', description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.', selected:'false'},
+    {color: '#d9f2e4', type: 'RESPEKTERAS', description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.', selected:'false'},
+    { color: '#d1f3f3',type: 'UTVECKLAS', description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.', selected:'false'},
     
+  ];
+
+  choses=[
+    {type:'OMSORG', choice:''},
+    {type:'TRYGGHET', choice:''},
+    {type:'MÅR BRA', choice:''},
+    {type:'FRITID', choice:''},
+    {type:'TILLHÖRIGHET', choice:''},
+    {type:'ANSVARSTAGANDE', choice:''},
+    {type:'RESPEKTERAS', choice:''},
+    {type:'UTVECKLAS', choice:''}
   ];
 
   guardianNbr: number=1;
@@ -77,6 +88,9 @@ radioChangeThree(event: MatRadioChange, data) {
   var obj = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];
   console.log(obj);
   
+  var obj1 = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];
+  console.log(obj1);
+
   obj.selected = event.value;
   console.log(this.ELEMENT_DATA);
 
@@ -88,7 +102,7 @@ radioChangefour(event: MatRadioChange, index1: number, index2: number) {
 }
 
   openDialog() {
-    /* console.log(this.date.toLocaleDateString());
+   // console.log(this.date.toLocaleDateString());
     console.log(this.discovererName);
     console.log(this.discovererOrganisation);
     console.log(this.discovererTitle);
@@ -96,28 +110,41 @@ radioChangefour(event: MatRadioChange, index1: number, index2: number) {
     console.log(this.name);
     console.log(this.personNbr);
     console.log(this.adress);
-     */
+    
 
     console.log(this.comments);
-/*     console.log(this.guardians[0][0]);
+    console.log(this.guardians[0][0]);
     console.log(this.guardians[0][1]);
     console.log(this.guardians[1][0]);
-    console.log(this.guardians[1][1]); */
+    console.log(this.guardians[1][1]);
 
-   /*  const dialogRef = this.dialog.open(CreateDiscoverCardDialogComponent, {
+    
+     const dialogRef = this.dialog.open(CreateDiscoverCardDialogComponent, {
       data:{
+       /*  date:this.date,
+        discovererName: this.discovererName,
+        discovererOrganisation: this.discovererOrganisation,
+        discovererTitle: this.discovererTitle,
+ */
         name: this.name,
         personNbr: this.personNbr,
-        adress: this.adress,
-        guardianName1: this.guardianName1
-      }
+        // adress: this.adress,
+        guardians: this.guardians,
+
+/*         personCotactName: this.personCotactName,
+        situationComment: this.situationComment,
+        comments:this.comments,
+
+        isMeasureTaken:this.isMeasureTaken,
+        isMeasureTakenComment: this.isMeasureTakenComment,
+ */      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if(result){
         this.store.dispatch(new fromRoot.Go({ path: ['discover-card'] }));
       }
-    }); */
+    });
   }
 
 
