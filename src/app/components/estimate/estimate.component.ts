af1e3de506a0d72b69e9b2426d821eeef5475fde
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-07 16:06:13 +0200

    ...

--- src/app/components/estimate/estimate.component.ts
+++ src/app/components/estimate/estimate.component.ts
@@ -154,53 +154,51 @@ export class EstimateComponent implements OnInit {
       {text:'Barnet får extra stöd och vård när det behövs', score:'care_4'}, {text:'Barnet bor i en miljö som är anpassad efter barnets behov samt främjar dess utveckling', score:'care_5'}, 
       {text:'Barnet har någon som ser till att hen är ren och lämpligt klädd efter årstid', score: 'care_6'}],
       stepControl: this.careFormGroup, id: "care-form", comment:''},
-      
-      { area: "TRYGGHET", question: [{text:'Barnet känner sig trygg hemma, i skolan, på nätet och i sin närmiljö',score: this.security_1}, 
-      {text: 'Barnet skyddas från kränkningar',score: this.security_2}, {text:'Barnet skyddas från fysiska faror och hälsofaror i och utanför hemmet',score: this.security_3},
-      {text:'Barnet visar förmåga att bedöma och hantera situationer som kan innebära en risk både för barnet själv och andra', score: this.security_4},
-      {text: 'Barnet litar på de som finns i dess närhet, såväl barn som vuxna', score:this.security_5}, {text: 'Barnet skyddas från att bli utnyttjat av andra', score:this.security_6},
-      {text: 'Barnet skyddas från kriminalitet och olaglig verksamhet',score: this.security_7},
-      {text: 'Barnet lever i en hemmiljö som är fri från missbruk, våld, försummelse och utnyttjande',score: this.security_8}],
+
+      { area: "TRYGGHET", question: [{text:'Barnet känner sig trygg hemma, i skolan, på nätet och i sin närmiljö',score: 'security_1'}, 
+      {text: 'Barnet skyddas från kränkningar',score: 'security_2'}, {text:'Barnet skyddas från fysiska faror och hälsofaror i och utanför hemmet',score: 'security_3'},
+      {text:'Barnet visar förmåga att bedöma och hantera situationer som kan innebära en risk både för barnet själv och andra', score: 'security_4'},
+      {text: 'Barnet litar på de som finns i dess närhet, såväl barn som vuxna', score:'security_5'}, {text: 'Barnet skyddas från att bli utnyttjat av andra', score:'security_6'},
+      {text: 'Barnet skyddas från kriminalitet och olaglig verksamhet',score: 'security_7'},
+      {text: 'Barnet lever i en hemmiljö som är fri från missbruk, våld, försummelse och utnyttjande',score: 'security_8'}],
       stepControl:this.securityFormGroup, id: "security-form", comment:''},
-      
-      { area: "MÅ BRA", question: [ {text:'Barnet är frisk och upprätthåller god fysisk och psykisk hälsa', score: this.feelgood_1},  {text:'Barnet har en hälsosam livsstil', score: this.feelgood_2}, 
-      {text: 'Barnet mår bra och ser positivt på framtiden', score: this.feelgood_3}, {text: 'Barnet kan hantera svårigheter och problem', score: this.feelgood_4},
-       {text: 'Barnet deltar vid kontroller/besök som ska ge barnet stöd/hjälp', score: this.feelgood_5}, 
-      {text:'Barnet uppvisar inte negativa eller destruktiva beteenden', score: this.feelgood_6}],
+            
+      { area: "MÅ BRA", question: [ {text:'Barnet är frisk och upprätthåller god fysisk och psykisk hälsa', score: 'feelgood_1'},  {text:'Barnet har en hälsosam livsstil', score: 'feelgood_2'}, 
+      {text: 'Barnet mår bra och ser positivt på framtiden', score: 'feelgood_3'}, {text: 'Barnet kan hantera svårigheter och problem', score: 'feelgood_4'},
+       {text: 'Barnet deltar vid kontroller/besök som ska ge barnet stöd/hjälp', score: 'feelgood_5'}, 
+      {text:'Barnet uppvisar inte negativa eller destruktiva beteenden', score:'feelgood_6'}],
       stepControl:this.feelgoodFormGroup , id: "feelgood-form", comment:''},
       
-      {area: "FRITID", question: [{text: 'Barnet och familjen är aktiva tillsammans och gör saker som barnet tycker är roligt', score: this.freetime_1}, 
-      {text: 'Barnet uppmuntras att vara aktiv utifrån sin förmåga, tex deltar i  lek, friluftsliv och idrottsaktiviteter', score: this.freetime_2}, 
-      {text: 'Barnet uppmuntras och ges förutsättningar för att utveckla egna intressen och att delta i aktiviteter som är stimulerande', score: this.freetime_3}],
+      {area: "FRITID", question: [{text: 'Barnet och familjen är aktiva tillsammans och gör saker som barnet tycker är roligt', score: 'freetime_1'}, 
+      {text: 'Barnet uppmuntras att vara aktiv utifrån sin förmåga, tex deltar i  lek, friluftsliv och idrottsaktiviteter', score: 'freetime_2'}, 
+      {text: 'Barnet uppmuntras och ges förutsättningar för att utveckla egna intressen och att delta i aktiviteter som är stimulerande', score: 'freetime_3'}],
       stepControl:this.freetimeFormGroup, id: "freetime-form", comment:''},
       
-      { area: "TILLHÖRIGHET", question: [{text: 'Barnet känner sig viktig och uppskattad av de som tar hand om hen', score: this.beloning_1}, 
-      {text: 'Barnet känner sig viktig och uppskattad av barn och vuxna som finn i dess närhet', score: this.beloning_2}, 
-      {text: 'Familjen har ett socialt nätverk som deltar aktivt i barnets liv', score: this.beloning_3}],
+      { area: "TILLHÖRIGHET", question: [{text: 'Barnet känner sig viktig och uppskattad av de som tar hand om hen', score: 'beloning_1'}, 
+      {text: 'Barnet känner sig viktig och uppskattad av barn och vuxna som finn i dess närhet', score: 'beloning_2'}, 
+      {text: 'Familjen har ett socialt nätverk som deltar aktivt i barnets liv', score: 'beloning_3'}],
       stepControl:this.beloningFormGroup, id: "beloning-form", comment:''},
       
-      { area: "ANSVARSTAGANDE", question: [{text: 'Barnet deltar i undervisningen i skolan', score: this.responsibility_1}, {text:'Barnet kan förstå och följa regler', score: this.responsibility_2}, 
-      {text: 'Barnet vet vad som är rätt och fel och agerar utifrån det', score: this.responsibility_3}, {text: 'Barnet tar ansvar för sina handlingar', score: this.responsibility_4}, 
-      {text: 'Barnet förstår vad som förväntas av hen och tar ansvar hemma, i skolan och i nära miljön.', score: this.responsibility_5}, 
-      {text: 'Barnet visar hänsyn och omtanke om andra',score:this.responsibility_6}, {text:'Barnet har bra förebilder i sin närhet', score:this.responsibility_7}],
+      { area: "ANSVARSTAGANDE", question: [{text: 'Barnet deltar i undervisningen i skolan', score: 'responsibility_1'}, {text:'Barnet kan förstå och följa regler', score: 'responsibility_2'}, 
+      {text: 'Barnet vet vad som är rätt och fel och agerar utifrån det', score: 'responsibility_3'}, {text: 'Barnet tar ansvar för sina handlingar', score: 'responsibility_4'}, 
+      {text: 'Barnet förstår vad som förväntas av hen och tar ansvar hemma, i skolan och i nära miljön.', score: 'responsibility_5'}, 
+      {text: 'Barnet visar hänsyn och omtanke om andra',score: 'responsibility_6'}, {text:'Barnet har bra förebilder i sin närhet', score:'responsibility_7'}],
       stepControl:this.responsibilityFormGroup , id: "responsibility-form", comment:''},
       
-      { area: "RESPEKTERAS", question: [{text: 'Barnet känner sig lyssnad till, tagen på allvar och är delaktig i viktiga vardagsbeslut', score: this.respekt_1}, 
-      {text: 'Barnet har en bra självkänsla och ser sig själv som värdeful', score: this.respekt_2},{text:  'Barnet känner att vänner och andra tror på dess förmåga och stöttar hen', score: this.respekt_3}, 
-      {text: 'Barnet känner sig inte retad, utsatt/utstött  eller kränkt av andra', score: this.respekt_4}], 
+      { area: "RESPEKTERAS", question: [{text: 'Barnet känner sig lyssnad till, tagen på allvar och är delaktig i viktiga vardagsbeslut', score: 'respekt_1'}, 
+      {text: 'Barnet har en bra självkänsla och ser sig själv som värdeful', score: 'respekt_2'},{text:  'Barnet känner att vänner och andra tror på dess förmåga och stöttar hen', score:'respekt_3'}, 
+      {text: 'Barnet känner sig inte retad, utsatt/utstött  eller kränkt av andra', score: 'respekt_4'}], 
       stepControl:this.respektFormGroup, id: "respekt-form", comment: ''},
       
-      { area: "UTVECKLAS", question: [{text: 'Barnet utvecklas och lär sig nya saker i olika miljöer', score: this.develop_1}, 
-      {text: 'Barnet är nyfiket och motiverat till att lära sig nya saker', score: this.develop_2}, 
-      {text: 'Barnet uppnår kunskapskraven för sin åldern', score: this.develop_3}, {text: 'Barnet har utvecklat förmågor för att klara av och hantera sin vardag', score: this.develop_4}, 
+      { area: "UTVECKLAS", question: [{text: 'Barnet utvecklas och lär sig nya saker i olika miljöer', score: 'develop_1'}, 
+      {text: 'Barnet är nyfiket och motiverat till att lära sig nya saker', score: 'develop_2'}, 
+      {text: 'Barnet uppnår kunskapskraven för sin åldern', score: 'develop_3'}, {text: 'Barnet har utvecklat förmågor för att klara av och hantera sin vardag', score: 'develop_4'}, 
       {text: 'Barnet har engagerade vuxna i sin närhet som stöttar hen i sin utveckling och i sitt lärande', score: this.develop_5}],
       stepControl:this.developFormGroup, id: "develop-form", comment:''},
     ];
   }
 
   changeScore(value: string, index1: number, index2: number){
-
-    
     this.questions[index1].question[index2].score= value;
     console.log(this.care_1);
   }
