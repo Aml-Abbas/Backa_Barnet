97530e2bb31c8a516a151099ea17228a8295c28a
Aml-Abbas <aml.abbas@hotmail.com>
2021-10-11 10:03:19 +0200

    ...

--- src/app/components/create-discover-card/create-discover-card.component.ts
+++ src/app/components/create-discover-card/create-discover-card.component.ts
@@ -15,14 +15,14 @@ import { MatRadioChange } from '@angular/material/radio';
 })
 export class CreateDiscoverCardComponent implements OnInit {
   ELEMENT_DATA = [
-    { color: '#ccd8ec', type: 'OMSORG', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.', selected:'false'},
-    { color: '#dbd9e6',type: 'TRYGGHET', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.', selected:'false'},
-    { color: '#ffdcee',type: 'MÅR BRA', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.', selected:'false'},
-    { color: '#fbdae1',type: 'FRITID', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.', selected:'false'},
-    { color: '#fee4d7',type: 'TILLHÖRIGHET', description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.', selected:'false'},
-    { color: '#fcedd6', type: 'ANSVARSTAGANDE', description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.', selected:'false'},
-    { color: '#d9f2e4', type: 'RESPEKTERAS', description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.', selected:'false'},
-    { color: '#d1f3f3',type: 'UTVECKLAS', description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.', selected:'false'},
+    { color: '#ccd8ec', type: 'OMSORG', id: 'care', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.', selected:'false'},
+    { color: '#dbd9e6',type: 'TRYGGHET',id: 'security', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.', selected:'false'},
+    { color: '#ffdcee',type: 'MÅR BRA',id: 'feel_good', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.', selected:'false'},
+    { color: '#fbdae1',type: 'FRITID',id: 'free_time', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.', selected:'false'},
+    { color: '#fee4d7',type: 'TILLHÖRIGHET',id: 'beloning', description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.', selected:'false'},
+    { color: '#fcedd6', type: 'ANSVARSTAGANDE',id: 'responsibility', description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.', selected:'false'},
+    { color: '#d9f2e4', type: 'RESPEKTERAS',id: 'respect', description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.', selected:'false'},
+    { color: '#d1f3f3',type: 'UTVECKLAS',id: 'develop', description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.', selected:'false'},
     
   ];
 
@@ -66,6 +66,8 @@ export class CreateDiscoverCardComponent implements OnInit {
       personNbrControl: ['', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
       adressControl: ['', Validators.required],
 
+      situationCommentControl:['', Validators.required],
+
     }); 
 
   }
@@ -76,20 +78,15 @@ export class CreateDiscoverCardComponent implements OnInit {
 
   radioChange(event: MatRadioChange) {
     this.isMeasureTaken = event.value;
-    console.log(this.isMeasureTaken);  
 }
 
 radioChangeThree(event: MatRadioChange, data) {
 
-  var obj = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];
-  console.log(obj);
-  
-  var obj1 = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];
-  console.log(obj1);
+  var obj = this.ELEMENT_DATA.filter(x => x.type == data.type)[0];  
+  var obj1 = this.choices.filter(x => x.type == data.type)[0];
 
+  obj1.choice= event.value;
   obj.selected = event.value;
-  console.log(this.ELEMENT_DATA);
-
 }
 
 radioChangefour(event: MatRadioChange, index1: number, index2: number) {
@@ -97,12 +94,24 @@ radioChangefour(event: MatRadioChange, index1: number, index2: number) {
   console.log(this.guardians);
 }
 
+checkChoices(): boolean{
+  var emptyChoice = true;
+  this.choices.forEach(element => {
+    if(element.choice==''){
+      emptyChoice= false;
+      }    
+  });
+  return emptyChoice;
+}
+
   openDialog() {
-   if(this.createDiscoveCardFormGroup.status== "INVALID"){
+   if(
+      this.createDiscoveCardFormGroup.status== "INVALID" ||
+      this.isMeasureTaken==undefined ||
+      this.checkChoices()==false){
     this.saveError='Du har missat att fylla i saker';
 
    }else{
-
     console.log(this.createDiscoveCardFormGroup.value.dateControl.toLocaleDateString());
     console.log(this.createDiscoveCardFormGroup.value.discovererNameControl);
     console.log(this.createDiscoveCardFormGroup.value.discovererOrganisationControl);
