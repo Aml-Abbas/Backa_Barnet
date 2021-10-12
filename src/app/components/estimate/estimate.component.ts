4fb0508523d8d6d9f275763e17527a57e1da7ed6
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-12 08:35:10 +0200

    ...

--- src/app/components/estimate/estimate.component.ts
+++ src/app/components/estimate/estimate.component.ts
@@ -4,23 +4,24 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
+  question: string;
   position: number;
   weight: number;
   symbol: string;
+  score: number;
   }
   
   const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {position: 1, question: 'Hydrogen',score: 1, weight: 1.0079, symbol: 'H'},
+    {position: 2, question: 'Helium',score: 1, weight: 4.0026, symbol: 'He'},
+    {position: 3, question: 'Lithium',score: 1, weight: 6.941, symbol: 'Li'},
+    {position: 4, question: 'Beryllium', score: 1,weight: 9.0122, symbol: 'Be'},
+    {position: 5, question: 'Boron', score: 1,weight: 10.811, symbol: 'B'},
+    {position: 6, question: 'Carbon',score: 1, weight: 12.0107, symbol: 'C'},
+    {position: 7, question: 'Nitrogen',score: 1, weight: 14.0067, symbol: 'N'},
+    {position: 8, question: 'Oxygen',score: 1, weight: 15.9994, symbol: 'O'},
+    {position: 9, question: 'Fluorine',score: 1, weight: 18.9984, symbol: 'F'},
+    {position: 10, question: 'Neon',score: 1, weight: 20.1797, symbol: 'Ne'},
     ];
     
   
@@ -34,7 +35,7 @@ export interface PeriodicElement {
 
 })
 export class EstimateComponent implements OnInit {
-  displayedColumns = ['name', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'star'];
+  displayedColumns = ['question', 'score', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'star'];
   dataSource = ELEMENT_DATA;
 
 
