bcb26be1d772894ee2270a2b2fe522957075f182
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-19 12:07:18 +0200

    ...

--- src/app/components/create-conversation-material/create-conversation-material.component.ts
+++ src/app/components/create-conversation-material/create-conversation-material.component.ts
@@ -3,25 +3,6 @@ import { Store } from '@ngrx/store';
 import * as fromState from '../../state';
 import * as fromRoot from '../../../app/state';
 
-export interface PeriodicElement {
-  question: string;
-  position: number;
-  score: number;
-  }
-  
-  const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, question: 'Hydrogen',score: 1},
-    {position: 2, question: 'Helium',score: 1},
-    {position: 3, question: 'Lithium',score: 1},
-    {position: 4, question: 'Beryllium', score: 1,},
-    {position: 5, question: 'Boron', score: 1},
-    {position: 6, question: 'Carbon',score: 1},
-    {position: 7, question: 'Nitrogen',score: 1},
-    {position: 8, question: 'Oxygen',score: 1},
-    {position: 9, question: 'Fluorine',score: 1},
-    {position: 10, question: 'Neon',score: 1},
-    ];
-
 
 @Component({
   selector: 'app-create-conversation-material',
@@ -29,10 +10,12 @@ export interface PeriodicElement {
   styleUrls: ['./create-conversation-material.component.scss']
 })
 export class CreateConversationMaterialComponent implements OnInit {
-  displayedColumns = ['question', 'score', 'position', 'position', 'star'];
-  dataSource = ELEMENT_DATA;
 
-/*   care_comment: string='';
+  guardianNbr: number=2;
+  selected = '2';
+
+
+  care_comment: string='';
   security_comment: string='';
   feel_good_comment: string='';
   free_time_comment: string='';
@@ -40,19 +23,58 @@ export class CreateConversationMaterialComponent implements OnInit {
   responsibility_comment: string='';
   respekt_comment: string='';
   develop_comment: string='';
- */
+ 
   saveError='';
-/*   scores = [
-    { area: "OMSORG", id: "care", question:'Jag har någon som bryr sig om mig', score:'', comment: this.care_comment },
-    { area: "TRYGGHET", id: "security", question:'Jag känner mig trygg', score:'', comment: this.security_comment},
-    { area: "MÅ BRA", id: "feel_good", question:'Jag mår bra', score:'', comment: this.feel_good_comment},
-    { area: "FRITID", id: "free_time", question:'Jag trivs med min fritid', score:'', comment: this.free_time_comment},
-    { area: "TILLHÖRIGHET", id: "beloning", question:'Jag får vara med', score:'', comment: this.beloning_comment},
-    { area: "ANSVARSTAGANDE", id: "responsibility", question:'Jag tar ansvar för mig själv och andra', score:'', comment: this.responsibility_comment},
-    { area: "RESPEKTERAS", id: "respekt", question:'Jag känner mig respekterad', score:'', comment: this.respekt_comment},
-    { area: "UTVECKLAS", id: "develop", question:'Jag gör mitt bästa', score:'', comment: this.develop_comment}
+   scores = [
+    { area: "OMSORG", id: "care",class: "care-class", question:'Jag har någon som bryr sig om mig', 
+    person_score:'', person_comment: this.care_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#003686'},
+
+    { area: "TRYGGHET", id: "security", class:'security-class', question:'Jag känner mig trygg', 
+    person_score:'', person_comment: this.security_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#353370'},
+
+    { area: "MÅ BRA", id: "feel_good", class:'feel_good-class', question:'Jag mår bra', 
+    person_score:'', person_comment: this.feel_good_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#e0448c'},
+
+    { area: "FRITID", id: "free_time", class:'free_time-class', question:'Jag trivs med min fritid', 
+    person_score:'', person_comment: this.free_time_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#df2d5b'},
+
+    { area: "TILLHÖRIGHET", id: "beloning", class:'beloning-class', question:'Jag får vara med', 
+    person_score:'', person_comment: this.beloning_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#eb612d'},
+
+    { area: "ANSVARSTAGANDE", id: "responsibility", class:'responsibility-class', question:'Jag tar ansvar för mig själv och andra', 
+    person_score:'', person_comment: this.responsibility_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#f79c2e'},
+
+    { area: "RESPEKTERAS", id: "respekt", class:'respekt-class', question:'Jag känner mig respekterad', 
+    person_score:'', person_comment: this.respekt_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#4ba562'},
+
+    { area: "UTVECKLAS", id: "develop", class:'develop-class', question:'Jag gör mitt bästa', 
+    person_score:'', person_comment: this.develop_comment,
+    guardian1_score:'', guardian1_comment: this.care_comment,
+    guardian2_score:'', guardian2_comment: this.care_comment,
+    color: '#31acaf'}
   ];
- */
+ 
   constructor(private store: Store<fromState.State>) { }
 
   ngOnInit(): void {
@@ -70,6 +92,7 @@ export class CreateConversationMaterialComponent implements OnInit {
       this.scores[6].score==''|| this.scores[7].score=='') {
     this.saveError='Du har glömt att välja ett betyg';
     }else{ */
+      console.log(this.scores);
       this.saveError='';
       if(nbr==1){
         console.log(nbr +" nbr is 1");
@@ -83,6 +106,20 @@ export class CreateConversationMaterialComponent implements OnInit {
       this.store.dispatch(new fromRoot.Go({ path: ['/conversation-material'] }));
 
     // }
+}
+
+changeGuardianNbr(nbr: number){
+  this.guardianNbr= nbr;
+}
+changePersonScore(nbr: number, i: number){
+  this.scores[i].person_score= String(nbr);
+}
+
+changeGuardian1Score(nbr: number, i: number){
+  this.scores[i].guardian1_score= String(nbr);
+}
+changeGuardian2Score(nbr: number, i: number){
+  this.scores[i].guardian2_score= String(nbr);
 }
 
 }
