6d2a9dbbb6e2dec70b0f23d24102ef52b08b93a5
Aml_Abbas <aml.abbas@hotmail.com>
2021-10-27 14:19:44 +0200

    ...

--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-event.component.scss']
 })
 export class CreateEventComponent implements OnInit {
+  createPlanFormGroup: FormGroup;
+  saveError='';
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createPlanFormGroup = this._formBuilder.group({
+      dateControl: ['', Validators.required],
+      nameControl: ['', [Validators.required, Validators.minLength(2)]],
+      proControl: ['', [Validators.required]],
+      eventControl: ['', [Validators.required]],
+      planControl: ['', [Validators.required]],
+
+    }); 
   }
 
+  save(){
+    if(this.createPlanFormGroup.status== "INVALID"){
+      this.saveError='Du har missat att fylla i saker';
+      }else{
+        this.saveError='';
+      }
+  }
 }
