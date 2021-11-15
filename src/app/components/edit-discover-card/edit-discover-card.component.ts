f68da0c5848f32b08e5bc9b6341c036ef009cc45
Aml_Abbas <aml.abbas@hotmail.com>
2021-11-15 13:23:04 +0100

    ...

--- src/app/components/edit-discover-card/edit-discover-card.component.ts
+++ src/app/components/edit-discover-card/edit-discover-card.component.ts
@@ -13,11 +13,43 @@ export class EditDiscoverCardComponent implements OnInit {
   current_card$= new Observable<Card | null>();
   card: Card;
 
+  isDirty = false;
+
+  ELEMENT_DATA = [
+    { color: '#ccd8ec', type: 'OMSORG', id: 'care', description: 'Barnet har vuxna i sin närhet som hen kan lita på och vända sig till.' },
+    { color: '#dbd9e6', type: 'TRYGGHET', id: 'security', description: 'Barnet skyddas från sådant som kan skada hen i och utanför hemmet.' },
+    { color: '#ffdcee', type: 'MÅR BRA', id: 'feel_good', description: 'Barnet har hälsosamma matvanor, god hygien och ett liv fritt från tobak, alkohol och narkotika.' },
+    { color: '#fbdae1', type: 'FRITID', id: 'free_time', description: 'Barnet har fritidsintresse med delaktight från vårdnadshavare eller annan trygg person i dess närhet.' },
+    { color: '#fee4d7', type: 'TILLHÖRIGHET', id: 'beloning', description: 'Barnet känner tillhörighet och uppskattning av personer som barne möter i sin vardag.' },
+    { color: '#fcedd6', type: 'ANSVARSTAGANDE', id: 'responsibility', description: 'Barnet förstår vas som förväntas av det i sin vardag, visar hänsyn och omtanke inför andra och följer givna regler.' },
+    { color: '#d9f2e4', type: 'RESPEKTERAS', id: 'respect', description: 'Barnet känner sig sedd, hörd och bekräftad av viktiga personer i sin vardag.' },
+    { color: '#d1f3f3', type: 'UTVECKLAS', id: 'develop', description: 'Barnet utvecklas i fas med sin ålder och tar förmågor att klara av det vardagliga livet.' },
+
+  ];
+
+  choices = [
+    { type: 'OMSORG', choice: 4 },
+    { type: 'TRYGGHET', choice: 4 },
+    { type: 'MÅR BRA', choice: 4 },
+    { type: 'FRITID', choice: 4 },
+    { type: 'TILLHÖRIGHET', choice: 4 },
+    { type: 'ANSVARSTAGANDE', choice: 4 },
+    { type: 'RESPEKTERAS', choice: 4 },
+    { type: 'UTVECKLAS', choice: 4 }
+  ];
+  
+  guardianNbr: number = 2;
+  selected = '2';
+
   constructor(private store: Store<fromState.State>) { }
 
   ngOnInit(): void {
     this.current_card$ = this.store.select(fromState.getCurrentCard);
 
+  }
+
+  changeGuardianNbr(nbr: number) {
+    this.guardianNbr = nbr;
   }
 
 }
