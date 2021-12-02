a8538eb8b100dde0be25c4350a11122fac1f209d
Aml_Abbas <aml.abbas@hotmail.com>
2021-12-02 10:52:26 +0100

    ...

--- src/app/components/need-compass/need-compass.component.ts
+++ src/app/components/need-compass/need-compass.component.ts
@@ -95,38 +95,65 @@ export class NeedCompassComponent implements OnInit {
 
   public radarChartData: ChartDataSets[] = [
     { data: [0, 4, 0, 4, 1, 4, 5, 0], label: 'Anna Lundberg', backgroundColor:'#003686'},
-    { data: [3, 3, 1, 2, 1, 3, 4, 4], label: 'Dalia Sundberg', backgroundColor:'#353370'},
-    { data: [4, 1, 2, 1, 1, 2, 3, 3], label: 'Amina Lundberg', backgroundColor:'#e0448c'},
-    { data: [0, 4, 0, 4, 1, 4, 5, 0], label: 'Hanna Lundberg', backgroundColor:'#df2d5b'},
-    { data: [3, 3, 1, 2, 1, 3, 4, 4], label: 'Dalia Lundberg', backgroundColor:'#eb612d'},
-    { data: [4, 1, 2, 1, 1, 2, 3, 3], label: 'Amina Lundberg', backgroundColor:'#f79c2e'},
-
   ];
 
-  public data: ChartDataSets[]=[
-    
-    { data: [0, 4, 0, 4, 1, 4, 5, 0], label: 'Karl Sundberg', backgroundColor:'#4ba562'},
-    { data: [1, 3, 1, 3, 1, 3, 1, 3], label: 'Gustav Sundberg', backgroundColor:'#31acaf'},
-    { data: [4, 1, 2, 1, 1, 2, 3, 3], label: 'Elin Sundberg', backgroundColor:'#2F4F4F'},
-    { data: [4, 4, 4, 4, 4, 4, 5, 5], label: 'Tony Sundberg', backgroundColor:'#228B22'},
-    { data: [0, 0, 1, 1, 1, 1, 1, 4], label: 'Adnan Sundberg', backgroundColor:'#FF69B4'},
-    { data: [4, 1, 2, 1, 1, 2, 3, 3], label: 'Maha Sundberg', backgroundColor:'#CD853F'},
+  questionIndex = new Map([
+    [ '1', 0 ],
+    [ "2", 1 ],
+    [ "3", 2 ],
+    [ '4', 3 ],
+    [ "5", 4 ],
+    [ "6", 5 ],
 
-    { data: [1, 2, 3, 4, 5, 0, 1, 2], label: 'Luna Svensson', backgroundColor:'#008080'},
-    { data: [3, 3, 1, 1, 1, 1, 4, 4], label: 'Mia Svensson', backgroundColor:'#FF6347'},
-    { data: [1, 1, 1, 1, 1, 1, 1, 1], label: 'Maram Svensson', backgroundColor:'#A0522D'},
-    { data: [0, 4, 0, 4, 0, 4, 0, 4], label: 'Manal Svensson', backgroundColor:'#808000'},
-    { data: [3, 3, 3, 2, 3, 3, 4, 4], label: 'Lama Svensson', backgroundColor:'#DAA520'},
-    { data: [4, 4, 2, 4, 1, 4, 3, 4], label: 'Nina Svensson', backgroundColor:'#B22222'},
-    { data: [5, 4, 5, 4, 5, 4, 5, 5], label: 'Kalle Svensson', backgroundColor:'#D2691E'},
+    [ '7', 0 ],
+    [ "8", 1 ],
+    [ "9", 2 ],
+    [ '10',3 ],
+    [ "11", 4 ],
+    [ "12", 5 ],
+    [ '59', 6 ],
+    [ "60", 7 ],
 
-  ];
+    [ '13', 0 ],
+    [ "14", 1 ],
+    [ "15", 2 ],
+    [ '16', 3 ],
+    [ "17", 4 ],
+    [ "18", 5 ],
+
+    [ '19', 0 ],
+    [ "20", 1 ],
+    [ "21", 2 ],
+
+    [ '22', 0 ],
+    [ "23", 1 ],
+    [ "24", 2 ],
+
+    [ '25', 0 ],
+    [ "26", 1 ],
+    [ "27", 2 ],
+    [ '28', 3 ],
+    [ "29", 4 ],
+    [ "30", 5 ],
+    [ "61", 6 ],
+
+    [ '31', 0 ],
+    [ "32", 1 ],
+    [ "33", 2 ],
+    [ '34', 3 ],
+
+    [ "35", 0 ],
+    [ "36", 1 ],
+    [ '37', 2 ],
+    [ "38", 3 ],
+    [ "39", 4 ],
+]);
 
   public radarChartType: ChartType = 'radar';
 
   conversationDates: string[]=[];
   estimateDates: string[]=[];
-  dates: string[];
+  dates = new Set()
 
   cards: CompassDataConversation[]= [];
   estimatecards: CompassDataEstimate[]= [];
@@ -142,22 +169,30 @@ export class NeedCompassComponent implements OnInit {
       this.personID= data?.personID?? '';
     });
 
-    this.getSetService.getCompass(this.personID).subscribe(data=>{
-        this.data.map((card: any)=>{
+    this.getSetService.getCompass(this.personID).subscribe(response_data=>{
+          response_data.map((card: any)=>{
+
         let questionID: string = card?.questionID ?? '';
         let questionTypeID: string = card?.questionTypeID ?? '';
-        let questionLevelID: string = card?.questionLevelID ?? '';
+        let questionLevelID= parseInt(card.questionLevelID)-1;
         let personID: string = card?.personID ?? '';
         let userID: string = card?.userID ?? '';
+        userID='Aml Nilsson Abbas';
+        let name= userID.split(' ');
+        let userName= name[0]+ ' '+ name[name.length-1];
+        while(userName.length<18){
+          userName+=' ';
+        }
         let grade: string = card?.grade ?? '';
         let comment: string = card?.comment ?? '';
         let gradedOn: string = card?.gradedOn ?? '';
+        let index= this.questionIndex.get(String(questionID))??0;
 
         //it is discovercard
         if(questionTypeID=='1'){
           if(!this.containsCard(gradedOn)){
             this.grades.push(parseInt(grade));
-             this.cards.push(new CompassDataConversation(gradedOn, userID, this.grades));
+             this.cards.push(new CompassDataConversation(gradedOn, userName, this.grades));
                this.grades= [];
                this.conversationDates.push(gradedOn.slice(0,10));
           }else{
@@ -169,26 +204,26 @@ export class NeedCompassComponent implements OnInit {
            }
         }else{
           let categories_data: any[]=[
-            {scores: {}},
-            {scores:{}},
-            {scores:{}},
-            {scores:{}},
-            {scores: {}},
-            {scores: {}},
-            {scores: {}},
-            {scores: {}}
+            {scores: [] },
+            {scores: [] },
+            {scores: [] },
+            {scores: [] },
+            {scores: [] },
+            {scores: [] },
+            {scores: [] },
+            {scores: [] }
           ];
           if(this.containsEstimate(gradedOn)){
             var found= false;
             this.estimatecards.forEach(element => {
               if(element.gradedOn== gradedOn && !false){
                 found= true;
-                element.grades[questionLevelID].scores.push(grade);
+                element.grades[questionLevelID].scores[index]= grade;
               }      
             });
           }else{
-            categories_data[questionLevelID].scores[0]= grade;
-            this.estimatecards.push(new CompassDataEstimate(gradedOn, userID, categories_data));
+            categories_data[questionLevelID].scores[0]=grade;
+            this.estimatecards.push(new CompassDataEstimate(gradedOn, userName, categories_data));
             this.estimateDates.push(gradedOn.slice(0,10));
           }  
         }
@@ -221,31 +256,31 @@ export class NeedCompassComponent implements OnInit {
     var colorIndex=0;
 
     if(this.selectedType=='2'){
-      this.dates=[];
+      this.dates.clear();
       this.estimateDates.forEach(element=>{
-        this.dates.push(element);
+        this.dates.add(element);
       });
 
       this.estimatecards.forEach(element=>{
         var averageGrades: number[]=[];
 
-        element.grades.forEach(scores=>{
+        element.grades.forEach(score=>{
           var nbr=0;
-          scores.forEach(number => {
-            nbr+= number;
-          });
-          averageGrades.push(nbr/scores.length);
+           for(var i=0; i<score.scores.length; i++){
+            nbr+= parseInt(score.scores[i]);
+          }
+          
+         averageGrades.push(Math.round(nbr/score.scores.length));
         });
-
         this.radarChartData.push({data: averageGrades, label: element.userName, backgroundColor: this.colors[colorIndex]});
         colorIndex++;
     });
     }else{
-      this.dates=[];
+      this.dates.clear();
       this.conversationDates.forEach(element=>{
-        this.dates.push(element);
+        this.dates.add(element);
     });
-    this.cards.forEach(element=>{
+    this.cards.forEach(element=>{  
       this.radarChartData.push({data: element.grades, label: element.userName, backgroundColor: this.colors[colorIndex]});
       colorIndex++;
     });
@@ -254,11 +289,39 @@ export class NeedCompassComponent implements OnInit {
 }
 
   onDateChange(){
+    this.radarChartData=[];
+    var colorIndex=0;
+
+    if(this.selectedType=='2'){
+      this.estimatecards.forEach(element=>{
+        var averageGrades: number[]=[];
+
+        element.grades.forEach(score=>{
+          var nbr=0;
+           for(var i=0; i<score.scores.length; i++){
+            nbr+= parseInt(score.scores[i]);
+          }
+          
+         averageGrades.push(Math.round(nbr/score.scores.length));
+        });
+        if(element.gradedOn.slice(0,10)== this.selectedDate){
+          this.radarChartData.push({data: averageGrades, label: element.userName, backgroundColor: this.colors[colorIndex]});
+          colorIndex++;  
+        }
+    });
+    }else{
+    this.cards.forEach(element=>{
+      if(element.gradedOn.slice(0,10)== this.selectedDate){
+      this.radarChartData.push({data: element.grades, label: element.userName, backgroundColor: this.colors[colorIndex]});
+      colorIndex++;
+      }
+    });
+
+  }
 
   }
 
   toggle(){
-    console.log('in the disable');
     this.chart.datasets.forEach(function(e,i){
       e.hidden= !e.hidden;
     });
