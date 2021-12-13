93570d294a6412e168ca2b7684a0d1e1039b2b4f
Aml_Abbas <aml.abbas@hotmail.com>
2021-12-13 10:13:56 +0100

    ...

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person } from 'src/app/models/Person';
-import * as fromRoot from '../../state';
 import { Store } from '@ngrx/store';
 import * as fromState from '../../state';
 import { GetSetService } from '../../services/get-set/get-set.service';
@@ -10,9 +9,6 @@ import { EventService } from 'src/app/services/event/event.service';
 import {Actions, ofType} from '@ngrx/effects';
 import * as eventAction from '../../state/actions/event.action';
 import { tap } from 'rxjs/operators';
-import { ConversationCard } from 'src/app/models/ConversationCard';
-import { EstimateCard } from 'src/app/models/EstimateCard';
-import { Card } from 'src/app/models/Card';
 import { User } from 'src/app/models/User';
 
 export interface Action {
@@ -26,6 +22,7 @@ export interface Action {
   status:string;
 }
 
+
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
@@ -33,20 +30,14 @@ export interface Action {
 })
 export class EventComponent implements OnInit {
 
-  actions: Action[] = [];
   
   current_person$= new Observable<Person | null>();
   events$: Observable<Event[]> = new Observable<Event[]>();
   personID:string;
-
-  ConversationCards: ConversationCard[]= [];
-  pcards: Promise<ConversationCard[]>= new Promise((resolve, reject) => { });
-
-  estimatecards: EstimateCard[]= [];
-  ecards: Promise<EstimateCard[]>= new Promise((resolve, reject) => { });
-
   current_user$: Observable<User| null> = new Observable<User| null>();
-  dcards: Promise<Card[]>= new Promise((resolve, reject) => { });
+
+  pActions: Promise<Action[]>= new Promise((resolve, reject) => { });
+  actions: Action[]= [];
 
   constructor(private getSetService: GetSetService,
     private store: Store<fromState.State>,
@@ -57,77 +48,25 @@ export class EventComponent implements OnInit {
     this.current_person$ = this.store.select(fromState.getCurrentPerson);
     this.current_person$.subscribe(data=>{
       this.personID = data?.personID ?? '';
-      this.pcards= this.getSetService.getConversationMaterial(this.personID);
-      this.ecards= this.getSetService.getEstimate(this.personID);
-    });
+     });
 
     this.current_user$ = this.store.select(fromState.getCurrentUser);
     this.current_user$.subscribe(data => {
       let userID: string = data?.userID ?? '';       
-       this.dcards= this.getSetService.getCards(userID);
+      this.pActions= this.eventService.getAllEvents(this.personID, userID);
     });
 
-    this.events$= this.eventService.getEvent(this.personID);
-    this.events$.subscribe(data=>{
-      data.map((action: Event)=>{
-        this.actions.push({title: 'Create insats', date: action.createdOn ,
-        actionDescription: action.actionDescription, eventDescription: action.eventDescription,
-        responsible: action.responsible, role: action.profession,
-        actionId: action.actionID ,status: action.status});
-      });
-      this.actions= this.actions.sort((a, b) => (a.date < b.date ? -1 : 1));
-    });
+    let actions= this.actions;
 
-    let cards= this.actions;
-    this.pcards.then(function (response) {
+    this.pActions.then(function (response) {
       
-      response.forEach((card: ConversationCard)=>{
-        cards.push({title: 'Create samtalsunderlag', date: card.gradedOn ,
-        actionDescription: card.person_scores.toString(), eventDescription: card.person_comments.toString(),
-        responsible: card.personID, role: card.personID,
-        actionId: card.id ,status: card.status});
+      response.forEach((action: Action)=>{
+        actions.push(action);
     });
     });
-
-    cards.forEach(element=>{
-      //this.ConversationCards.push(element);
-      this.actions.push(element);
-      this.actions= this.actions.sort((a, b) => (a.date < b.date ? -1 : 1));
-      console.log(this.actions);
-    });
-
-    let estimatecards= this.actions;
-    this.ecards.then(function (response) {
-      
-      response.forEach((card: EstimateCard)=>{
-          estimatecards.push({title: 'Create skattning', date: card.gradedOn ,
-          actionDescription: card.grades.toString(), eventDescription: card.grades.toString(),
-          responsible: card.userName, role: card.userID,
-          actionId: card.userID ,status: card.status});    
-      });
-    });
-
-    estimatecards.forEach(element=>{
-      //this.estimatecards.push(element);
-      this.actions.push(element);
-
-    });
-
-    let discoverCards= this.actions;
-
-    this.dcards.then(function (response) {
-      
-      response.forEach((card: Card)=>{
-        discoverCards.push({title: 'Create upptäckarkort', date: card.gradedOn ,
-        actionDescription: card.grades.toString(), eventDescription: card.grades.toString(),
-        responsible: card.userName, role: card.userTitle,
-        actionId: card.id ,status: card.status});
-    });
-    });
-    discoverCards.forEach(element=>{
+    actions.forEach(element=>{
       this.actions.push(element);
     });
-
 
     this.actions$.pipe(
       ofType(eventAction.UPDATE_EVENT_SUCCESS),
@@ -145,4 +84,4 @@ export class EventComponent implements OnInit {
    this.store.dispatch(new fromState.UpdateEvent(actionID));
 
     }
-}
+    }
--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -2,13 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Event } from 'src/app/models/Event';
+import axios from 'axios';
+import { GetSetService } from '../../services/get-set/get-set.service';
+import { ConversationCard } from 'src/app/models/ConversationCard';
+import { EstimateCard } from 'src/app/models/EstimateCard';
+import { Card } from 'src/app/models/Card';
+import {map} from "rxjs/operators";
+
+export interface Action {
+  title: string;
+  date: string;
+  actionDescription: string;
+  eventDescription: string;
+  responsible: string;
+  role:string;
+  actionId: string;
+  status:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private getSetService: GetSetService) { }
 
   createEvent(eventJson: any) {
     return this.http.post('https://func-ykbb.azurewebsites.net/api/event/create?code=zH/btVHzaGqpESkmr3SCic2B4CBLIvGRTXaJu7z/vVsYDb1t1Mw61Q==', eventJson);
@@ -26,4 +44,87 @@ export class EventService {
     return this.http.get<Event[]>('https://func-ykbb.azurewebsites.net/api/event/'+personId+'?code=4BSzbYC1YIm9Qxgocsd1A/Z9dwyuHw1SiGNRVEhIFD3LxbaoLAKV1g==');
   }
   
+  async getAllEvents(personId: string, userId: string): Promise<Action[]> {
+
+   var actions: Action[] = [];
+   var pcards= this.getSetService.getConversationMaterial(personId);
+   var ecards= this.getSetService.getEstimate(personId);
+   var events$= this.getEvent(personId);
+   var dcards= this.getSetService.getCards(userId);
+
+    await axios.get('https://func-ykbb.azurewebsites.net/api/card/'+userId+'?code=bbdIBAbikn/AMydOBvxm69FyKFhRfS4fxUb55SaSz0TfK/cjnxiYEw==')
+    .then(function (response) {
+      events$.subscribe(data=>{
+        data.map((action: Event)=>{
+          actions.push({title: 'Create insats', date: action.createdOn ,
+          actionDescription: action.actionDescription, eventDescription: action.eventDescription,
+          responsible: action.responsible, role: action.profession,
+          actionId: action.actionID ,status: action.status});
+        });
+      });
+  
+      let cards= actions;
+      pcards.then(function (response) {
+        
+        response.forEach((card: ConversationCard)=>{
+          cards.push({title: 'Create samtalsunderlag', date: card.gradedOn ,
+          actionDescription: card.person_scores.toString(), eventDescription: card.person_comments.toString(),
+          responsible: card.personID, role: card.personID,
+          actionId: card.id ,status: card.status});
+      });
+      });
+  
+      cards.forEach(element=>{
+        actions.push(element);
+      });
+  
+      let estimatecards= actions;
+      ecards.then(function (response) {
+        
+        response.forEach((card: EstimateCard)=>{
+            estimatecards.push({title: 'Create skattning', date: card.gradedOn ,
+            actionDescription: card.grades.toString(), eventDescription: card.grades.toString(),
+            responsible: card.userName, role: card.userID,
+            actionId: card.userID ,status: card.status});    
+        });
+      });
+  
+      estimatecards.forEach(element=>{
+        actions.push(element);
+  
+      });
+  
+      let discoverCards= actions;
+  
+      dcards.then(function (response) {
+        
+        response.forEach((card: Card)=>{
+          discoverCards.push({title: 'Create upptäckarkort', date: card.gradedOn ,
+          actionDescription: card.grades.toString(), eventDescription: card.grades.toString(),
+          responsible: card.userName, role: card.userTitle,
+          actionId: card.id ,status: card.status});
+      });
+      });
+      discoverCards.forEach(element=>{
+        actions.push(element);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+    });
+     return actions.sort((a,b) => compare(a, b));
+  }
+
 }
+function compare(a: Action, b: Action): number {
+  if (a.date<b.date) {
+    return -1;
+  }
+  if (a.date > b.date) {
+    return 1;
+  }
+  return 0;
+}
+
